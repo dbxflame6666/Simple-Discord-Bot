39483f3d3622974fa9a0624ea2792379c72cd9c5
dbxflame6666 <106920457+dbxflame6666@users.noreply.github.com>
2023-01-31 12:00:14 +0100

    Create ButtonintoView.py

---
+++ VerifySystem/Views/ButtonintoView.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext  import commands
+from datetime import datetime
+import asyncio
+from Views.verifyModalView import verifyModal
+
+
+class Buttonintomodal(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+
+    @discord.ui.button(
+        label='Next Step',
+        style=discord.ButtonStyle.red,
+        custom_id='nS'
+    )
+    async def callback1(self, button, interaction):
+        await interaction.response.send_modal(verifyModal(title='Put in the correct code!'))
