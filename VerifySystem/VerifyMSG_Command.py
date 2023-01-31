88bf70edc0b3c49083c711306e5823c16606e9ce
dbxflame6666 <106920457+dbxflame6666@users.noreply.github.com>
2023-01-31 11:59:42 +0100

    Update VerifyMSG_Command.py

--- VerifySystem/VerifyMSG_Command.py
+++ VerifySystem/VerifyMSG_Command.py
@@ -10,7 +10,7 @@ from discord.ext  import commands
 from discord.commands import slash_command
 from datetime import datetime
 import asyncio
-from Views.verifyModalView import VerifyView
+from Views.verifyModalView import verifyModal
 
 class verifymsg(commands.Cog):
     def __init__(self, client):
