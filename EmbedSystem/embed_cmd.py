33a380bcb9df12488eed47955f32606bf2bfdd1e
dbxflame6666 <106920457+dbxflame6666@users.noreply.github.com>
2023-02-07 21:51:18 +0100

    Create embed_cmd.py

---
+++ EmbedSystem/embed_cmd.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+from discord.commands import slash_command
+from EmbedSystem.Views.dbxmodal import dbxModal
+
+class Sayembed(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @slash_command()
+    async def embed(self, ctx):
+        modal = dbxModal(title="Create an Embed")
+        await ctx.send_modal(modal)
+
+
+def setup(client):
+    client.add_cog(Sayembed(client))
