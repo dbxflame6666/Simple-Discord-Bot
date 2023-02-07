95f3bd43d1d76f6ddb38c793329da215d06c7b61
dbxflame6666 <106920457+dbxflame6666@users.noreply.github.com>
2023-02-07 21:45:05 +0100

    Update TicketMSG_Command.py

--- TicketSystem/TicketMSG_Command.py
+++ TicketSystem/TicketMSG_Command.py
@@ -8,7 +8,7 @@ from discord.ext  import commands
 from discord.commands import slash_command
 from datetime import datetime
 import asyncio
-from Views.MainTicketView import TicketViewCreateTicket
+from TicketSystem.Views.MainTicketView import TicketViewCreateTicket
 
 class TicketMSG(commands.Cog):
     def __init__(self, client):
