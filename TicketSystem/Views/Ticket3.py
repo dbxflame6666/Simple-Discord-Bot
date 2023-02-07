3775b521a1b447f973b3230b967c183a70e7b469
dbxflame6666 <106920457+dbxflame6666@users.noreply.github.com>
2023-02-07 21:45:42 +0100

    Update Ticket3.py

--- TicketSystem/Views/Ticket3.py
+++ TicketSystem/Views/Ticket3.py
@@ -1,6 +1,6 @@
 import discord
 from datetime import datetime
-from Views.TicketButtonsEm import TicketViewButtonEmMSG
+from TicketSystem.Views.TicketButtonsEm import TicketViewButtonEmMSG
 
 class PartnerModal(discord.ui.Modal):
     def __init__(self, *args, **kwargs):
