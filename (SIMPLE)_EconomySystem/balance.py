0bb81165310f7774b227e43bbdb613d8b90d48fe
dbxflame6666 <106920457+dbxflame6666@users.noreply.github.com>
2023-02-07 22:00:17 +0100

    Update balance.py

--- (SIMPLE)_EconomySystem/balance.py
+++ (SIMPLE)_EconomySystem/balance.py
@@ -1,10 +1,8 @@
-import discord  ,  aiosqlite  ,  random
+import discord  ,  aiosqlite
 from discord.ext import commands  ,  bridge
 from datetime import datetime
-from (SIMPLE)_EconomySystem.Views import events
 
-
-class crime(commands.Cog):
+class Balance(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.DB = "fun.db"
@@ -32,49 +30,38 @@ class crime(commands.Cog):
         return result[0]
 
 
-    @bridge.bridge_command(description='Do a crime to earn a lot of Coins')
-    @commands.cooldown(1, 180, commands.BucketType.user)
-    async def crime(self, ctx):
+    @bridge.bridge_command(
+        name='balance',
+        description='Shows your Current Balance',
+        aliases=['bal']
+        )
+    async def balance(self, ctx):
         eco = discord.utils.get(self.client.emojis, name='economye')
 
-        Chance = random.randint(1, 100)
-        if Chance > 60:
-            coins = random.randint(20, 80)
+        cash = await self.get_cash(ctx.author.id)
+        bank = await self.get_bank(ctx.author.id)
+        total = await self.get_bank(ctx.author.id) + await self.get_cash(ctx.author.id)
 
-            eventP = random.choice(events.crimeP(coins, eco))
+        em = discord.Embed(
+            color=discord.Color.green(),
+            timestamp=datetime.now()
+        )
+        em.add_field(
+            name='Cash:',
+            value=f' {eco} {cash:,}'
+        )
+        em.add_field(
+            name='Bank:',
+            value=f' {eco} {bank:,}'
+        )
+        em.add_field(
+            name='Total:',
+            value=f'{eco} {total:,}'
+        )
+        em.set_author(name=f"{ctx.author.name}'s Balance", icon_url=f'{ctx.author.avatar}')
 
-            em = discord.Embed(
-                description=f'{eventP}',
-                color=discord.Color.green(),
-                timestamp=datetime.now()
-            )
-            em.set_author(name=f"{ctx.author.name}", icon_url=f'{ctx.author.avatar}')
-
-            async with aiosqlite.connect(self.DB) as db:
-                async with db.execute("UPDATE economy SET cash = cash + ? WHERE user_id = ?", (coins, ctx.author.id,)) as cursor:
-                    await db.commit()
-
-            await ctx.respond(embed=em)
-
-
-        else:
-            coinsN = random.randint(10, 70)
-
-            eventN = random.choice(events.crimeN(coinsN, eco))
-
-            em2 = discord.Embed(
-                description=f'{eventN}',
-                color=discord.Color.red(),
-                timestamp=datetime.now()
-            )
-            em2.set_author(name=f"{ctx.author.name}", icon_url=f'{ctx.author.avatar}')
-
-            async with aiosqlite.connect(self.DB) as db:
-                async with db.execute("UPDATE economy SET cash = cash - ? WHERE user_id = ?", (coinsN, ctx.author.id,)) as cursor:
-                    await db.commit()
-
-            await ctx.respond(embed=em2)
+        await ctx.respond(embed=em)
 
 
 def setup(client):
-    client.add_cog(crime(client))
+    client.add_cog(Balance(client))
